661a14a6f6a0200e83fde7c930365c76eaea6f39
Diego Suárez <dsuarezrobledo@gmail.com>
2021-07-21 09:20:02 -0500

    agrego base de datos proyecto integrador

--- 03-ejercicio/moc.sql
+++ 03-ejercicio/moc.sql
@@ -4,15 +4,24 @@ nombre          VARCHAR(50) NOT NULL,
 apellidos       VARCHAR(50) NOT NULL,
 email           VARCHAR(50) NOT NULL,
 password        VARCHAR(50) NOT NULL,
-apodo           VARCHAR(50),
+desarrollador_id INT NOT NULL,
+empleador_id    INT NOT NULL,
 idiomas         VARCHAR(200),
 fecha           DATE NOT NULL,
-CONSTRAINT      pk_usuarios PRIMARY KEY(id),
-CONSTRAINT      uq_email UNIQUE(email)
+CONSTRAINT      uq_email UNIQUE(email),
+CONSTRAINT      pk_usuarios PRIMARY KEY(id)
 );
-
-CREATE TABLE desarrollador(
+CREATE TABLE desarrolladores(
 id              INT AUTO_INCREMENT NOT NULL,
-portafolio      VARCHAR(500) /* Preguntar que datatype seria una caarpeta donde se suben diferentes archivos */
-
+portafolio      VARCHAR(500), /* Preguntar que datatype seria una caarpeta donde se suben diferentes archivos */
+datos_bancarios VARCHAR(12),
+CONSTRAINT      uq_datos_bancarios UNIQUE(datos_bancarios),
+CONSTRAINT      pk_desarrollador PRIMARY KEY(id)
 );
+CREATE TABLE empleadores(
+id              INT AUTO_INCREMENT NOT NULL,
+datos_bancarios VARCHAR(12),
+CONSTRAINT      pk_empleadores PRIMARY KEY(id)
+);
+ALTER TABLE usuarios ADD CONSTRAINT fk_usuario_desarrollador FOREIGN KEY(desarrollador_id) REFERENCES desarrolladores(id);
+ALTER TABLE usuarios ADD CONSTRAINT fk_usuario_empleador FOREIGN KEY(empleador_id) REFERENCES empleadores(id);
--- operaciones-basicas/where.sql
+++ operaciones-basicas/where.sql
@@ -22,4 +22,32 @@ No es como      not like
 
 /* Ejemplos */
 
-SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) = '2020' OR  YEAR(fecha) = '2021';
+/*Si queremos consultar a los usuarios de un año específico, utilizamos YEAR(fecha)*/
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) = '2021';
+
+/*Si agregamos el operador de compración OR, nos dará el resultado de todos los valores que cumplan con la condición*/
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) = '2021' OR  YEAR(fecha) = '2020';
+
+/*Con el operador de comparación >=, indicamos que buscamos todos los valores que sean mayor o igual que el valor declarado*/
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) >= '2019';
+
+/*Si queremos especificar que un valor es nulo, utilizamos IS NULL*/
+SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) != '2019' OR ISNULL (fecha);
+
+/*EJEMPLO DE LOGIN*/
+
+/*En el caso del login, agregamos los datos como nombre, apellido y contraseña, y utilizamos el operador de comparación =*/
+SELECT nombre, apellidos FROM usuarios WHERE email = 'carlos@eduardo.com' AND password = '1234';
+
+
+/*Usamos LIKE para filtrar los datos que cumplan con una condición*/
+SELECT nombre, apellidos FROM usuarios WHERE nombre LIKE '%a%';
+
+/*Ejemplos de consultas con LIKE/*
+/*Sintaxis: SELECT * FROM usuarios WHERE nombre LIKE '%a%';*/
+
+/*Utilizamos o% para indicar que el texto debe comenzar con el valor declarado*/
+/*Utilizamos %o% para indicar que el elemento debe estar en cualquier lugar del texto*/
+/*Utilizamos %o para indicar que el texto debe terminar con el valor declarado*/
+
+SELECT * FROM usuarios WHERE nombre LIKE '%o'
